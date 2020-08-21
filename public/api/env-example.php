937304553695faed11d48da6f7882b5c0b4c2e83
Thomas Lu <thomas@Thomass-MacBook-Air.local>
2020-08-21 04:06:56 -0700

    use old env from our forefathers

--- public/api/env-example.php
+++ public/api/env-example.php
@@ -1,6 +1,6 @@
 <?php 
-$host = "localhost"; 
-$user = "root"; 
-$password = "root"; 
-$dbname = "website"; 
+define("DB_HOST",		"localhost");
+define("DB_USER",		"root");
+define("DB_PASSWORD",		"root");
+define("DB_DATABASE",		"website");
 ?>
--- public/api/make_con.php
+++ public/api/make_con.php
@@ -1,6 +1,6 @@
 <?php
 require('env-example.php');
 
-$con = mysqli_connect($host, $user, $password, $dbname);
+$con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 
 ?>
